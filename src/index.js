0a7f3eb0a0d18e8583de033a9ffabedc1f0dfc91
Ramzi Bouchedoub <ramzibouchedoub@Ramzis-MacBook-Air.local>
2023-04-17 11:28:54 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+
 app.get('/notes' , (req , res) => {
     fs.readFile(__dirname + '/'+ "notes.json", 'utf-8',(err , data) => {
         if (err) {
